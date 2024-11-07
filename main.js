e52caee264dde6fb00770259f32871d5d2bc0384
Piotr <piotrwejda@gmail.com>
2024-11-08 00:26:56 +0100

    second commit

--- main.js
+++ main.js
@@ -2,6 +2,13 @@ var config = {
     type: Phaser.Auto,
     width: 800,
     height: 600,
+    physics:{
+        default: 'arcade',
+        arcade:{
+            //gravity:{y: 5},
+            debug:false
+        }
+    },
     scene: {
         preload: preload,
         create: create,
@@ -12,10 +19,31 @@ var config = {
 var game = new Phaser.Game(config);
 
 function preload(){
+    this.load.image('space','assets/space.jpg');
+    this.load.image('spaceship','assets/spaceship.png');
+    this.load.image('asteroid','assets/asteroid.png');
+    this.load.image('explosion','assets/explosion.png');
+    this.load.image('bullet','assets/bullet.png');
 }
 
 function create(){
+    this.add.image(config.width/2,config.height/2,'space');
+    player = this.physics.add.sprite(config.width/2,config.height-100,'spaceship').setScale(0.15,0.15);
 }
 
 function update(){
+    document.addEventListener('keydown',function(e){
+        if(e.key == 'a' || e.key == "ArrowLeft"){
+            player.body.angularVelocity = -100;
+        }else if(e.key == 'd' || e.key == "ArrowRight"){
+            player.body.angularVelocity = 100;
+        }else{
+            player.body.angularVelocity = 0;
+        }
+        if(e.key == 'w' || e.key == 'ArrowUp'){
+            player.setVelocity(Math.cos(player.rotation)*100,Math.sin(player.rotation)*100);
+        }else{
+            player.setAcceleration(0);
+        }
+    })
 }
